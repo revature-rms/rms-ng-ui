634d70478bd123eb520ff9edd2a856a82f0da695
capolimia <capolimia@gmail.com>
2020-10-07 14:53:48 -0400

    creating login compontent, auth service, and principal model. ALso used ng materials for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,37 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    HttpClientModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppModule } from '../app.module';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 
 import { LoginComponent } from './login.component';
 
@@ -8,7 +12,12 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ], 
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        AppModule
+    ]
     })
     .compileComponents();
   });
@@ -17,9 +26,20 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain login title', () => {
+    const headerEl: HTMLElement = fixture.nativeElement;
+    const p = headerEl.querySelector('header');
+    fixture.detectChanges();
+    expect(p.textContent).toContain('Login');
+  })
+
+  
 });
