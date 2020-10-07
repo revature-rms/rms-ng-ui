f1a69a86be308215a26684531d01fc046335d331
capolimia <capolimia@gmail.com>
2020-10-06 20:44:05 -0400

    Added login component, auth service, and principal object.

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  submitted = false;
+  loading = false;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  
+  }
+
+  get formFields() {
+    return this.loginForm.controls;
+  }
+  
+  login() {
+
+    this.submitted=true;
+    let username = this.formFields.email.value;
+    let password = this.formFields.password.value;
+
+    if (this.loginForm.invalid) return;
+
+    this.loading = true;
+    
+    this.authService.authenticate(username, password);
+                        // .subscribe(
+                        //   () => {
+                        //     this.loading = false;
+                        //     console.log('login-successful');
+                        //     // this.router.navigate(['/home']);
+                        //   },
+                        //   err => {
+                        //     console.log(err);
+                        //     this.loading = false;
+                        //     this.submitted = false;
+                        //   }
+                        // );
+
+  }
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { Principal } from '../models/principal';
+import { map } from 'rxjs/operators';
+
+import { environment as env } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private currentUserSubject: BehaviorSubject<Principal>
+  currentUser$: Observable<Principal>
+
+
+  constructor(private http: HttpClient) { 
+    console.log('Instantiating Authservice');
+    this.currentUserSubject = new BehaviorSubject<Principal>(null);
+    this.currentUser$ = this.currentUserSubject.asObservable();
+    console.log('Authservice instantiation complete');
+  }
+
+  get currentUserValue() {
+    return this.currentUserSubject.value;
+  }
+
+  authenticate(username: string, password: string) {
+    let credentials = { username, password };
+    // return this.http.post(API_URL/auth, credentials, {
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   observe: 'response'
+    // }).pipe(
+    //   map(resp => {
+    //     let principal = resp.body as Principal;
+    //     this.currentUserSubject.next(principal);
+    //   })
+    // );
+  }
+
+  // logout() {
+  //   this.http.get(API_URL/auth);
+  //   this.currentUserSubject.next(null);
+    
+  // }
+
+  isAuthentication(): boolean {
+    return !!this.currentUserValue;
+  }
+}
