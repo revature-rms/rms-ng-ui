ede4f420b2697ff482ba9caffdefdb9182501eb7
Daniela Yacobucci Lapaitis <dyacobucci47@gmail.com>
2020-10-07 13:04:02 -0400

    generated empty component

---
+++ src/app/employee-detail-view/employee-detail-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-detail-view',
+  templateUrl: './employee-detail-view.component.html',
+  styleUrls: ['./employee-detail-view.component.scss']
+})
+export class EmployeeDetailViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
