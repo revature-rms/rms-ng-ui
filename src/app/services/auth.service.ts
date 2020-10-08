29460d390fbf8a103d016b2d3b2e6c77b8d40003
Daniela Yacobucci Lapaitis <dyacobucci47@gmail.com>
2020-10-08 16:39:00 -0400

    Changed principal reference from being in models folder to being in dtos folder

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import { Principal } from '../models/principal';
+import { Principal } from '../dtos/principal';
 import { map } from 'rxjs/operators';
 
 import { environment as env } from '../../environments/environment';
