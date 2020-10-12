e33917fe0a8c8302a72e408d093ccc075474f93c
EPaetow <60556354+EPaetow@users.noreply.github.com>
2020-10-12 10:49:25 -0500

    building component detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {TsmDashboardComponent} from './tsm-dashboard/tsm-dashboard.component'
+import {TsmDashboardComponent} from './tsm-dashboard/tsm-dashboard.component';
+import { BuildingDetailsComponent } from './building-details/building-details.component';
+
 
 const routes: Routes = [
   {path: '', redirectTo: '/tsm-dashboard', pathMatch: 'full'},
-  {path: 'tsm-dashboard', component: TsmDashboardComponent}
+  {path: 'tsm-dashboard', component: TsmDashboardComponent} , 
+  {path: 'building-details', component: BuildingDetailsComponent}
+
 ];
 
 @NgModule({
