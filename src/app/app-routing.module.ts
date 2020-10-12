16f0f6bc0cfa8c5dd50b1552e4e4411e1e3e8e99
Daniela Yacobucci Lapaitis <dyacobucci47@gmail.com>
2020-10-12 12:09:39 -0400

    added EmployeeDetailviewcomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeDetailViewComponent } from './employee-detail-view/employee-detail-view.component';
 import {TsmDashboardComponent} from './tsm-dashboard/tsm-dashboard.component'
 
 const routes: Routes = [
   {path: '', redirectTo: '/tsm-dashboard', pathMatch: 'full'},
-  {path: 'tsm-dashboard', component: TsmDashboardComponent}
+  {path: 'tsm-dashboard', component: TsmDashboardComponent},
+  {path: 'tsm-employee-details', component: EmployeeDetailViewComponent}
 ];
 
 @NgModule({
