44cafbe982eb73a2104f2d6008cc7b8ab7fd096d
capolimia <capolimia@gmail.com>
2020-10-12 12:27:12 -0400

    starting edit basics, this branch may not  be necessary.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RoomDetailsComponent } from './room-details/room-details.component';
+import { RoomEditComponent } from './room-edit/room-edit.component';
 import {TsmDashboardComponent} from './tsm-dashboard/tsm-dashboard.component'
 
 const routes: Routes = [
   {path: '', redirectTo: '/room-details', pathMatch: 'full'},
   {path: 'tsm-dashboard', component: TsmDashboardComponent},
   {path: 'login', component: LoginComponent },
-  {path: 'room-details', component: RoomDetailsComponent}
+  {path: 'room-details', component: RoomDetailsComponent},
+  {path: 'room-edit', component: RoomEditComponent}
 ];
 
 @NgModule({
---
+++ src/app/room-edit/room-edit.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { Batch } from '../dtos/batch';
+import { Room } from '../dtos/room';
+import { WorkOrder } from '../dtos/workOrder';
+import { RoomService } from '../services/room.service';
+import { WorkOrderService } from '../services/work-order.service';
+
+@Component({
+  selector: 'app-room-edit',
+  templateUrl: './room-edit.component.html',
+  styleUrls: ['./room-edit.component.scss']
+})
+export class RoomEditComponent implements OnInit {
+
+  rooms: Room[];
+  currentRoom: Room = new Room();
+  roomBatch: Batch = new Batch();
+  workOrders: WorkOrder[];
+  dataSource:any[]=[];
+  displayedColumns: string[] = ['id', 'createdDateTime', 'resolvedDateTime', 'category', 'contactEmail', 'creator', 'resolver'];
+
+  constructor(private roomService: RoomService, private workOrderService: WorkOrderService) { }
+
+  async ngOnInit() {
+
+    await this.roomService.getRooms().then(
+      res => {
+        console.log('get-rooms-successful');
+        this.rooms = <Room[]> res;
+        this.currentRoom = this.rooms[0];
+        this.roomBatch = this.currentRoom.batch;
+      },
+      err => {
+        console.log(err);
+      });
+
+    await this.workOrderService.getWorkOrder().subscribe(
+        res => {
+          console.log('get-work-orders-successful');
+          this.workOrders = res;
+          this.dataSource = this.workOrders;
+        },
+        err => {
+          console.log(err);
+        });
+  
+  }
+
+}
