ce2c9d943e35cb8f28f693d0f60bbc239fdb145b
Daniela Yacobucci Lapaitis <dyacobucci47@gmail.com>
2020-10-12 12:54:44 -0400

    solved merge conflicts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,19 +9,16 @@ import {TsmDashboardComponent} from './tsm-dashboard/tsm-dashboard.component'
 const routes: Routes = [
   {path: '', redirectTo: '/tsm-dashboard', pathMatch: 'full'},
   {path: 'tsm-dashboard', component: TsmDashboardComponent},
-<<<<<<< HEAD
   {path: 'tsm-employee-details', component: EmployeeDetailViewComponent}
-=======
   // {path: 'admn-dashboard', component: AdmnDashboardComponent}, //admin dashboard
   // {path: 'bmngr-dashboard', component: BmngrDashboardComponent}, //building manager dashboard
   // {path: 'trnr-dashboard', component: TrnrDashboardComponent}, //trainer dashboard
   // {path: 'rooms', component: RoomDetailComponent},             //room detail
   // {path: 'buildings', component: BuildingDetailComponent},     //building detail
   // {path: 'users', component: UserDetailComponent},             //user detail
-  // {path: 'employees', component: EmployeeDetailComponent},     //employee detail
+  // {path: 'employees', component: EmployeeDetailComponent},     //employee detail//named it differently. might have to refactor
   // {path: 'resources', component: ResourceDetailComponent},     //resource detail
 
->>>>>>> 5c969ae194911a0c324989edbf77309c8cf7c0ee
 ];
 
 @NgModule({
