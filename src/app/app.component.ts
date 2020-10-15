6adcf41680fa48ec4a6c35af3972c08d149a08fa
Daniela Yacobucci Lapaitis <dyacobucci47@gmail.com>
2020-10-15 15:54:13 -0400

    app component has not access to the current route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+
+constructor(private router: Router ) {
+
+  router.events.subscribe((url:any) => {
+  
+  
+  if(url instanceof NavigationEnd){
+    console.log('yaay');
+    console.log(router.url);
+  };
+  
+  //console.log(Event + "<- THE EVENT " + url + "this this the url?" + router.url);
+
+  });
+
+}
+
   title = 'rms-ng-ui';
   opened = true;
+
+
 }
